be5c68fa654478dbd11c198668117badb59fd815
Sergio Tomas <sergiota.cs@gmail.com>
2022-05-22 18:39:16 +0200

    First commit, vanilla setup

---
+++ src/components/GifApp.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const GifApp = () => {
+
+    const categories = ['One Punch', 'Naruto', 'Dragon Ball']
+
+    return(
+        <>
+            <h2>Gif APP</h2>
+            <hr />
+        </> 
+    )
+    
+}
+
+export default GifApp;
